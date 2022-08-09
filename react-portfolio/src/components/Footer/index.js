ee0396d2c102852bb8cd380b574a1ebd5378a3ae
Megan Gedde <hoglu070@d.umn.edu>
2022-08-08 22:13:31 -0500

    added icons in footer

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import mailIcon from '../../assets/email.jpg';
+import linkedinIcon from '../../assets/linkedin.jpg';
+import githubIcon from '../../assets/github.jpg';
 
 function Footer() {
     return (
     <footer id="contact-me">
             <ul className="icons">
                 <li>
-                    <a>612.751.5779</a>
+                    <a href="mailto:meggedde@gmail.com">
+                        <img src={mailIcon} className="pic"/>
+                    </a>
                 </li>
                 <li>
-                    <a href="mailto:meggedde@gmail.com">MegGedde@gmail.com</a>
+                    <a href="https://github.com/MegGedde">
+                        <img src={githubIcon} className="pic"/>
+                    </a>
                 </li>
                 <li>
-                    <a href="https://github.com/MegGedde">GitHub</a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/megan-gedde-1731a495/">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/megan-gedde-1731a495/">
+                        <img src={linkedinIcon} className="pic"/>
+                    </a>
                 </li>
             </ul> 
     </footer>
