f2c8a5738506cd74ff676e5740359eabdf5a641b
Megan Gedde <hoglu070@d.umn.edu>
2022-08-08 21:55:56 -0500

    added additional projects

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -4,19 +4,32 @@ function Project() {
     const cardInfo = [
       {
           title: "Run Buddy",
-          text: "Website designed for fitness company called Run Buddy",
           site: "https://meggedde.github.io/run-buddy/",
           github: "https://github.com/MegGedde/run-buddy",
           tech: "Technologies Used: HTML, CSS",
           image: require("../../assets/run-buddy.jpg")
       },
       {
-          title: "Run Buddy",
-          text: "Website designed for fitness company called Run Buddy",
-          site: "https://meggedde.github.io/run-buddy/",
-          github: "https://github.com/MegGedde/run-buddy",
-          tech: "Technologies Used: HTML, CSS"
-      }]
+          title: "SunFund",
+          site: "https://meggedde.github.io/SunFund/",
+          github: "https://github.com/MegGedde/SunFund",
+          tech: "Technologies Used: HTML / CSS / JavaScript ",
+          image: require("../../assets/sunbuddy.jpg")
+      },
+      {
+        title: "Unleashed",
+        site: "https://unleashed-pets-finder.herokuapp.com/",
+        github: "https://github.com/MegGedde/Unleashed",
+        tech: "Technologies Used: JavaScript / CSS / Handlebars / Express / Node / SQL ",
+        image: require("../../assets/unleashed.jpg")
+    },
+    {
+        title: "project 3",
+        text: "Website designed for yuioyucompany called Run Buddy",
+        site: "https://meggedde.githubuigody/",
+        github: "https://github.comiouyude/run-buddy",
+        tech: "Technologies Used: HTML, CSS"
+    }]
 
    return (
     <div>
@@ -25,19 +38,19 @@ function Project() {
           <div className="flexBox">
               {cardInfo.map((cardInfo) => (
                   <div className="card">
-              <a href={cardInfo.site} className="card">
-                  <img src={cardInfo.image} className="image" alt="picture"/>
-              <h2 className="card-header">{cardInfo.title} </h2>
-              <div className="card-body">
-              <p >{cardInfo.text}</p>
-              <a href={cardInfo.github}>
-                  <p>
-                      Link to Code
-                  </p>
-              </a>
-              <p>{cardInfo.tech}</p>
-              </div>
-              </a>
+                    <h2 className="card-header">{cardInfo.title} </h2>
+                    <a href={cardInfo.site}>
+                        <img src={cardInfo.image} className="image" alt="webpage"/>
+                    </a>
+                    <div className="card-body">
+                        <p >{cardInfo.text}</p>
+                        <a href={cardInfo.github} className="github">
+                            <p className="github">
+                                Link to Github Code
+                            </p>
+                        </a>
+                        <p>{cardInfo.tech}</p>
+                    </div>
               </div>
               ))}
           </div>
