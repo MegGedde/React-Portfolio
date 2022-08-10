e95a213308feb80aecf9becd736bfa0942beb40a
Megan Gedde <hoglu070@d.umn.edu>
2022-08-09 21:10:59 -0500

    minor changes

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -9,17 +9,17 @@ function Footer() {
             <ul className="icons">
                 <li>
                     <a href="mailto:meggedde@gmail.com">
-                        <img src={mailIcon} className="pic"/>
+                        <img src={mailIcon} className="pic" alt="mail"/>
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/MegGedde">
-                        <img src={githubIcon} className="pic"/>
+                        <img src={githubIcon} className="pic" alt="github"/>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/megan-gedde-1731a495/">
-                        <img src={linkedinIcon} className="pic"/>
+                        <img src={linkedinIcon} className="pic" alt="linkedin"/>
                     </a>
                 </li>
             </ul> 
