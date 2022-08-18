ae1d9054fef6fc231d41d3c07daefb35ce2443b2
Megan Gedde <hoglu070@d.umn.edu>
2022-08-17 20:06:21 -0500

    commented out contact section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Project from './components/Project';
 import About from './components/About';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 import Resume from './components/Resume';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         "Junior Software Engineer leveraging professional Human Resources background to build programs and problem solve. Recently earned a certificate in full stack development from the University Coding through the University of Minnesota, with newly developed skills in JavaScript, CSS, Node.js, SQL, NoSQL, Express.js, React, RESTful API’s, ORM, MVC and responsive web design. Known as an innovative problem solver, my strong collaboration, communication, and attention to detail skills lead me to being passionate about the code being correct and having a purpose. With each project, my aim is to make the biggest impact on the user experience.  I look forward to leveraging my skills as part of a fast-paced, quality-driven team to build better experiences.",
     },
     { name: "Portfolio", description: "Projects that I have worked on." },
-    { name: "Contact", description: "Please share your information and I will reach out to you. If you prefer, you may reach out to me as well." },
+    // { name: "Contact", description: "Please share your information and I will reach out to you. If you prefer, you may reach out to me as well." },
     {
       name: "Resume",
       description: "coming soon",
@@ -33,9 +33,9 @@ function App() {
     else if(currentCategory.name === "Resume") {
       return <Resume></Resume>
     }
-    else if(currentCategory.name === "Contact") {
-      return <Contact></Contact>
-    }
+    // else if(currentCategory.name === "Contact") {
+    //   return <Contact></Contact>
+    // }
   }
 
   return (
